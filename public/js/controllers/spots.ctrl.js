1d53eb1af8a800606a2028773016c97cabe2fa6b
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-03-02 10:37:44 +0000

    functions fixed for displaying objects from the database

--- public/js/controllers/spots.ctrl.js
+++ public/js/controllers/spots.ctrl.js
@@ -8,17 +8,19 @@ function SpotsController (Spot, Auth, $stateParams, $state, $http) {
 	self.all = []
 	self.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
+	self.id = 'ViTYx85uUUc'
+
 	self.createSpot = function() {
 		Spot.create({
-				uid: Auth.$getAuth().uid,
-				spot: {
-					title: self.newSpot.title,
-					body: self.newSpot.body,
-					video: self.newSpot.video,
-					location: self.newSpot.location
-				}
-			}).then(function (spot) {
-				console.log(spot)
+			uid: Auth.$getAuth().uid,
+			spot: {
+				title: self.newSpot.title,
+				body: self.newSpot.body,
+				video: self.newSpot.video,
+				location: self.newSpot.location
+			}
+		})
+		.then(function (spot) {
 				self.newSpot = {}
 				$state.go('my-spots')
 			})
@@ -31,7 +33,6 @@ function SpotsController (Spot, Auth, $stateParams, $state, $http) {
 	self.showSpots = function() {
 		Spot.getAll()
 		.then(function(res) {
-			console.log(res)
 			console.log(res)
 			self.all = res.data.spots
 			console.log(self.all)
@@ -62,7 +63,6 @@ function SpotsController (Spot, Auth, $stateParams, $state, $http) {
 		self.video = ""
 		self.location = ""
 	}
-	
 
 
 }
--- public/js/controllers/users.ctrl.js
+++ public/js/controllers/users.ctrl.js
@@ -1,8 +1,24 @@
 angular
-	.module('Users')
+	.module('SpotPop')
 	.controller('UsersController', UsersController)
 
-function UsersController (User, $stateParams, $state) {
+function UsersController (User, $stateParams, $state, Auth) {
 	var self = this;
-	
+
+	// self.email = {}
+
+	self.getUser = function () {
+
+		var uid = Auth.$getAuth().uid
+
+		User.get(uid)
+			.then(function(res) {
+				console.log(res)
+				self.tagName = res.data.user.tagName
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+	}
+
 }
