d33d46023c8f775e6199aa0f7d80cc7aca240256
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-03-02 10:38:53 +0000

    removed unused path

--- public/js/factories/user.fact.js
+++ public/js/factories/user.fact.js
@@ -2,12 +2,29 @@ angular
 	.module('SpotPop')
 	.factory('User', userFactory)
 
-function userFactory(API, $resource) {
-	return $resource(API + '/users/:id', { id: '@id' }, { 
-		'getAll': { method: 'GET', isArray: false },
-		'get': { method: 'GET' },
-		'create': { method: 'POST'},
-		'delete': { method: 'DELETE'},
-		'update': { method: 'PATCH'}
-	})
+function userFactory(API, $http) {
+	// return $resource(API + '/users/:id', { id: '@id' }, { 
+	// 	'getAll': { method: 'GET', isArray: false },
+	// 	'get': { method: 'GET' },
+	// 	'create': { method: 'POST'},
+	// 	'delete': { method: 'DELETE'},
+	// 	'update': { method: 'PATCH'}
+	// })
+	return {
+		getAll: function() {
+			return $http.get(API + '/users')
+		},
+		get: function(id) {
+			return $http.get(API + '/users/' + id)
+		},
+		create: function(newUser) {
+			return $http.post(API + '/users', newSpot)
+		},
+		delete: function(id) {
+			return $http.delete(API + '/users/' + id)
+		},
+		update: function(id, updatedUser) {
+			return $http.patch(API + '/users/' + id, updatedUser)
+		}
+	}
 }
